ddc76ed7e6fa825e7cc30a0e3c365cfec479b5cd
Melecio Ponte <pontezambrano@gmail.com>
2014-01-24 13:52:43 -0430

    Se disminuye la velocidad Se arreglan detalles del txt

--- credits.py
+++ credits.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+qo
 import curses
 import time
 import os
@@ -6,6 +9,7 @@ import os
 pantalla = curses.initscr()
 pantalla.erase()
 pantalla.scrollok(True)
+velocidad = 0.4
 
 #Se obtienen las dimensiones de la pantalla
 maxx, maxy = pantalla.getmaxyx()
@@ -20,22 +24,6 @@ lineas.reverse()
 credFile.close()
 numlineas = len(lineas)
 
-# # #Se crea un pad para mostrar los creditos
-# cpad = curses.newpad(20,20)
-# cpad.border(0)
-# cpad.scrollok(True)
-# cpad.idlok(True)
-
-# cpad.refresh(0,0, 3,5, 20,20)
-
-
-
-# for linea in lineas:
-#     cpad.addstr(18,1,linea)
-#     cpad.scroll(1)
-#     time.sleep(1)
-#     cpad.refresh(0,0, 3,5, 20,20)
-# cpad.getch()
 try:
     os.system("timidity Title01.mid > /dev/null 2>/dev/null &")
     for i in range(0,numlineas+maxx):
@@ -45,17 +33,11 @@ try:
             linea = lineas.pop()
             linea = linea[:-1]
             pantalla.addstr(maxx-1, (maxy-1-linea.__len__())/2, linea)
-        time.sleep(0.3)
+        time.sleep(velocidad)
     os.system("killall timidity > /dev/null 2>/dev/null ")    
 except KeyboardInterrupt:
     curses.endwin()
-
-# pad = curses.newpad(40,60)
-# pad_pos = 0
-# pad.refresh(pad_pos, 0, 5, 5, 10, 60)
-
-# time.sleep(2)
-
+    os.system("killall timidity > /dev/null 2>/dev/null ")    
 
 curses.endwin()
 
