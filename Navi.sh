9fcc1eb4e2f19e853054d98f43d82c2c9d4fec86
Sofia Isabel <sofiabravo103@gmail.com>
2014-01-24 13:27:06 -0430

    Navi funcionando con trap pero sin el efecto de mover a ganondorf

--- Navi.sh
+++ Navi.sh
@@ -1,5 +1,71 @@
 MENSAJE_READ="[Enter para continuar] "
 
+function trapear {
+    trap insultar SIGHUP
+    trap insultar SIGINT
+    trap insultar SIGQUIT
+    trap insultar SIGILL
+    trap insultar SIGTRAP
+    trap insultar SIGABRT
+    trap insultar SIGBUS
+    trap insultar SIGFPE
+#
+    trap insultar SIGUSR1
+    trap insultar SIGSEGV
+    trap insultar SIGUSR2
+    trap insultar SIGPIPE
+    trap insultar SIGALRM
+    trap insultar SIGTERM
+    # trap insultar SIGSTKFLT
+    # trap insultar SIGCHLD
+    # trap insultar SIGCONT
+    trap insultar SIGSTOP
+    trap insultar SIGTSTP
+    # trap insultar SIGTTIN
+    # trap insultar SIGTTOU
+    # trap insultar SIGURG
+    # trap insultar SIGXCPU
+    # trap insultar SIGXFSZ
+    # trap insultar SIGVTALRM
+    # trap insultar SIGPROF
+    # trap insultar SIGWINCH
+    # trap insultar SIGIO
+    # trap insultar SIGPWR
+    # trap insultar SIGSYS
+    # trap insultar SIGRTMIN
+    # trap insultar SIGRTMIN+1
+    # trap insultar SIGRTMIN+2
+    # trap insultar SIGRTMIN+3
+    # trap insultar SIGRTMIN+4
+    # trap insultar SIGRTMIN+5
+    # trap insultar SIGRTMIN+6
+    # trap insultar SIGRTMIN+7
+    # trap insultar SIGRTMIN+8
+    # trap insultar SIGRTMIN+9
+    # trap insultar SIGRTMIN+10
+    # trap insultar SIGRTMIN+11
+    # trap insultar SIGRTMIN+12
+    # trap insultar SIGRTMIN+13
+    # trap insultar SIGRTMIN+14
+    # trap insultar SIGRTMIN+15
+    # trap insultar SIGRTMAX-14
+    # trap insultar SIGRTMAX-13
+    # trap insultar SIGRTMAX-12
+    # trap insultar SIGRTMAX-11
+    # trap insultar SIGRTMAX-10
+    # trap insultar SIGRTMAX-9
+    # trap insultar SIGRTMAX-8
+    # trap insultar SIGRTMAX-7
+    # trap insultar SIGRTMAX-6
+    # trap insultar SIGRTMAX-5
+    # trap insultar SIGRTMAX-4
+    # trap insultar SIGRTMAX-3
+    # trap insultar SIGRTMAX-2
+    # trap insultar SIGRTMAX-1
+    # trap insultar SIGRTMAX
+    echo ""
+}
+
 function inicio {
     clear
     cat dibujos/navi.txt
@@ -17,6 +83,13 @@ function inicio {
     cat dibujos/navi.txt
 }
 
+function dormir {
+    for i in `seq 1 100000`
+    do
+	echo ""
+    done
+}
+
 function go_evil {
     cat dibujos/majora.txt
     sleep 0.3
@@ -27,9 +100,19 @@ function go_evil {
     done
     clear
     cat dibujos/ganondorf.txt
-    break
+    trapear    
+    while true
+    do
+	p=""
+    done
 }
 
+function insultar {
+    clear
+    cat dialogos/dialogo.txt
+    sleep 1
+    cat dibujos/ganondorf.txt
+}
 
 function final {
     clear 
@@ -53,7 +136,7 @@ function final {
 	    printf "\n\n\n"
 	    read -p "$MENSAJE_READ"       
 	    break
-	elif [ $respuesta = "SI" -o $respuesta = "si"]
+	elif [  $respuesta = "SI" -o $respuesta = "si" ]
 	then
 	    break
 	else
@@ -95,7 +178,6 @@ function entregar_mascara {
 	    ;;
     esac
 }
-
 
 inicio
 
